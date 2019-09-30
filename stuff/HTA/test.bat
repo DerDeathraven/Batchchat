a2bc55cdf770026fceb43a10803ede3c505b0f17
DerDeathraven <55985926+DerDeathraven@users.noreply.github.com>
2019-09-30 17:53:50 +0200

    v6

--- stuff/HTA/test.bat
+++ stuff/HTA/test.bat
@@ -5,11 +5,15 @@ setlocal
 :start
 
 for /F "delims=" %%a in ('mshta.exe "%~F0"') do set "HTAreply=%%a"
-
-
+if "%HTAreply%" == "clear" goto clear
+	
 
 echo %username%: %HTAreply%>>log
 goto :start
+:clear
+del log
+echo Ahh nice a fresh Page >>log
+goto start
 -->
 <html>
 	<head>
@@ -22,7 +26,12 @@ goto :start
 	</head>
 	<body>
 		<label for="input" >Nachricht:</label>
-		<input type="text" id="nach" name="input" onkeydown="key(event)">
+		<input type="text" id="nach" name="input" onkeydown="key(event)" autofocus>
+		<script >
+			window.onload = function() {
+  		 document.getElementById("nach").focus();
+}
+		</script>
 		<span onclick="closeHTA('2')" onmouseover="red()" onmouseleave="white()" id="close">X</span>
 		<script language="JavaScript" src="script.js">
 		</script>
