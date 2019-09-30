f554d1af1158f56d3cc3fbf2da301733548f3f1c
BuildTools <unconfigured@null.spigotmc.org>
2019-09-30 15:34:40 +0200

    lel

--- stuff/HTA/test.bat
+++ stuff/HTA/test.bat
@@ -13,22 +13,22 @@ pause
 echo %username%: %HTAreply%>>log
 goto :start
 -->
-<HTML>
-	<HEAD>
+<html>
+	<head>
 		<HTA:APPLICATION SCROLL="no" SYSMENU="no" >
 		
-		<TITLE>HTA Buttons</TITLE>
+		<title>HTA Buttons</title>
 		<link rel=stylesheet href="style.css">
 		
 
-	</HEAD>
-	<BODY>
+	</head>
+	<body>
 		<label for="input" >Nachricht:</label>
 		<input type="text" id="nach" name="input" onkeydown="key(event)">
 		<div id="close"><span onclick="closeHTA('2')">X</span></div>
-	<SCRIPT language="JavaScript" src="script.js">
-		</SCRIPT>
-	</BODY>
+	<script language="JavaScript" src="script.js">
+		</script>
+	</body>
 
 
-</HTML>
+</html>
